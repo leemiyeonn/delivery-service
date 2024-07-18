9152cf704d6a88c540225b405736371553d974a5
leemiyeon <lemingul7@gmail.com>
2024-07-18 14:31:20 +0900

    feat: [user] add token model

---
+++ api/src/main/java/org/delivery/api/domain/token/model/TokenDto.java
@@ -0,0 +1,19 @@
+package org.delivery.api.domain.token.model;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDto {
+
+    private String token;
+
+    private LocalDateTime expiredAt;
+
+}
