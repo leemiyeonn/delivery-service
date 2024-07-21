da764362ea4bcd420a70b6e87e448ced6ad61121
leemiyeon <lemingul7@gmail.com>
2024-07-21 16:57:32 +0900

    feat: [storemenu] add storemenu logic

---
+++ db/src/main/java/org/delivery/db/storemenu/enums/StoreMenuStatus.java
@@ -0,0 +1,11 @@
+package org.delivery.db.storemenu.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum StoreMenuStatus {
+    REGISTERED("registered"),
+    UNREGISTERED("unregistered");
+
+    private String description;
+}
